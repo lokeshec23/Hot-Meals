85889fec2c9e269f2f224bf628ab28c3ac8c9f01
lokeshec23 <lokeshs.ece2019@dce.edu.in>
2023-09-08 18:37:09 +0530

    update

--- js/index.js
+++ js/index.js
@@ -1,14 +1,2 @@
-let initialValue = 1;
-let price  = document.querySelectorAll('.price')
-let addBtn = document.querySelectorAll(".add");
-for (let i = 0; i < addBtn.length; i++) {
-  addBtn[i].addEventListener("click", () => {
-    addBtn[i].style.display = "none";
-    const div = document.createElement("div");
-    const plus = document.createElement("button");
-    const minus = document.createElement("button");
-    const orderValue = document.createElement("span");
-    const Element  = div.append(plus, orderValue, minus)
-    p
-  });
-}
+
+ 
