fc1fc11e097b93fb86de0735f4a27d64d1f2d158
lokeshec23 <lokeshs.ece2019@dce.edu.in>
2023-09-10 17:51:08 +0530

    update

--- js/addToCart.js
+++ js/addToCart.js
@@ -28,12 +28,22 @@ function loadContent() {
   qtyElements.forEach((input) => {
     input.addEventListener("change", changeQty);
   });
+  //Product cart
+  let cartBtns = document.querySelectorAll(".add-cart");
+  cartBtns.forEach((btn) => {
+    btn.addEventListener("click", addCart);
+  });
+
+  updateTotal();
 }
 
 //Remove Item function
 function removeItem() {
   if (confirm("Are Your Sure To Remove")) {
+    let title = this.parentElement.querySelector(".cart-food-title").innerHTML;
+    itemList = itemList.filter((el) => el.title != title);
     this.parentElement.remove();
+    loadContent();
   }
 }
 
@@ -41,5 +51,81 @@ function removeItem() {
 function changeQty() {
   if (isNaN(this.value) || this.value < 1) {
     this.value = 1;
+  }
+  loadContent();
+}
+
+let itemList = [];
+
+//Add Cart
+function addCart() {
+  let food = this.parentElement;
+  let title = food.querySelector(".food-title").innerHTML;
+  let price = food.querySelector(".food-price").innerHTML;
+  let imgSrc = food.querySelector(".food-img").src;
+
+  let newProduct = {
+    title,
+    price,
+    imgSrc,
+  };
+
+  //Check Product already exist in cart
+
+  if (itemList.find((el) => el.title == newProduct.title)) {
+    alert("This product is already added to your cart");
+    return;
+  } else {
+    itemList.push(newProduct);
+  }
+
+  let newProductElement = createCartProduct(title, price, imgSrc);
+  let element = document.createElement("div");
+  element.innerHTML = newProductElement;
+  let cartBasket = document.querySelector(".cart-content");
+  cartBasket.append(element);
+  loadContent();
+}
+
+function createCartProduct(title, price, imgSrc) {
+  return `
+            <div class="cart-box">
+            <img src="${imgSrc}" class="cart-img" alt="">
+            <div class="detail-box">
+                <div class="cart-food-title">${title}</div>
+                <div class="price-box">
+                    <div class="cart-price">${price}</div>
+                    <div class="cart-amt">${price}</div>
+                </div>
+                <input type="number" value="1" min="1" class="cart-quantity">    
+            </div>
+            <i class="fa-solid fa-trash cart-remove" style="color: #ffb329;"></i>
+          </div>
+      `;
+}
+
+function updateTotal() {
+  const cartItems = document.querySelectorAll(".cart-box");
+  const totalValue = document.querySelector(".total-price");
+  let total = 0;
+  cartItems.forEach((product) => {
+    let priceElement = product.querySelector(".cart-price");
+    let price = parseFloat(priceElement.innerHTML.replace("₹ ", ""));
+    let qty = product.querySelector(".cart-quantity").value;
+    total += price * qty;
+    product.querySelector(".cart-amt").innerText = "₹ " + price * qty;
+  });
+
+  totalValue.innerHTML = "₹ " + total;
+
+  // ADD product count in cart icon
+  const cartCount = document.querySelector(".cart-count");
+  let count = itemList.length;
+  cartCount.innerHTML = count;
+
+  if (count == 0) {
+    cartCount.style.display = "none";
+  } else {
+    cartCount.style.display = "block";
   }
 }
