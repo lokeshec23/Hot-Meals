fe8ae4e4417c4048a714e22e2e66dacf19c305fd
lokeshec23 <lokeshs.ece2019@dce.edu.in>
2023-09-10 13:47:26 +0530

    update

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
+const addCart = document.querySelectorAll(".add-cart");
+const cartCount = document.getElementById("cart-count");
 
- 
+let orderItem = 0;
+
+for (let i = 0; i < addCart.length; i++) {
+  addCart[i].addEventListener("click", () => {
+    window.location.href = "addToCart.html";    
+  });
+}
